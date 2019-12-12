06143a487a79de43429eecda8e1b6dada3dd444e
Edward <lued2018@gmail.com>
2019-12-12 11:08:31 -0500

    started PWM

---
+++ PWM/Inc/nvic.h
@@ -0,0 +1,21 @@
+#ifndef __NVIC__
+#define __NVIC__
+
+#include "stm32f103.h"
+
+// nested vectored interrupt controller (pg 118 Programming Manual) //
+typedef struct nvic_t {
+    unsigned volatile long iser[3];	// 0x0 to 0xB - interrupt set-enable registers
+	unsigned volatile long icer[3];	// 0xC to 0x18 - interrupt clear-enable registers
+	unsigned volatile long ispr[3];	// 0x18 to 0x24 - interrupt set-pending registers
+	unsigned volatile long icpr[3];	// 0x24 to 0x30 - interrupt clear-pending registers
+	unsigned volatile long iabr[3];	// 0x30 to 0x3C - interrupt active bit registers
+	// unsigned volatile long ipr[3];	// 0x3C to 0x48 - interrupt priority registers
+	// unsigned volatile long stir;	// 0x48 to 0x54 - software trigger interrupt register
+} nvic_t;
+
+nvic_t *nvic;
+
+void nvic_enable(unsigned char irq_pos);
+
+#endif
